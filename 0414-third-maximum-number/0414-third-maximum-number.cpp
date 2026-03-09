b5e246158006f41cf6b341ab97190598e638c392
Vibha Bhat <vibhabhat0410@gmail.com>
2026-03-09 12:59:01 +0530

    Time: 0 ms (100%), Space: 12.8 MB (92.73%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+
+        long max1 = LONG_MIN, max2 = LONG_MIN, max3 = LONG_MIN;
+
+        for(int num : nums) {
+
+            if(num == max1 || num == max2 || num == max3)
+                continue;
+
+            if(num > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            }
+            else if(num > max2) {
+                max3 = max2;
+                max2 = num;
+            }
+            else if(num > max3) {
+                max3 = num;
+            }
+        }
+
+        if(max3 == LONG_MIN)
+            return max1;
+
+        return max3;
+    }
+};
