f7ad3e2e44efaafac3992ef51416f2bc4f03f92b
Vibha Bhat <vibhabhat0410@gmail.com>
2026-03-19 23:57:01 +0530

    Time: 0 ms (100%), Space: 8.1 MB (30.78%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maximumSwap(int num) {
+
+        string s = to_string(num);
+        vector<int> last(10);
+
+        // Store last index of each digit
+        for(int i = 0; i < s.size(); i++) {
+            last[s[i] - '0'] = i;
+        }
+
+        // Try to swap
+        for(int i = 0; i < s.size(); i++) {
+
+            for(int d = 9; d > s[i] - '0'; d--) {
+
+                if(last[d] > i) {
+                    swap(s[i], s[last[d]]);
+                    return stoi(s);
+                }
+            }
+        }
+
+        return num;
+    }
+};
