2dfe972094b5791dc75df9983918be116808f733
Vibha Bhat <vibhabhat0410@gmail.com>
2026-03-21 19:51:01 +0530

    Time: 0 ms (100%), Space: 8.5 MB (72.19%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        long long INT_MAX_LL = pow(2, 31) - 1;
-        long long INT_MIN_LL = -pow(2, 31);
 
-        if (dividend == INT_MIN_LL && divisor == -1) return INT_MAX_LL;
+        if(dividend == INT_MIN && divisor == -1)
+            return INT_MAX;
 
-        long long a = llabs(dividend);
-        long long b = llabs(divisor);
+        long long a = llabs((long long)dividend);
+        long long b = llabs((long long)divisor);
+
         long long result = 0;
 
-        // Bitwise division
-        while (a >= b) {
-            long long temp = b, multiple = 1;
-            while (a >= (temp << 1)) {
+        while(a >= b) {
+
+            long long temp = b;
+            long long multiple = 1;
+
+            while(a >= (temp << 1)) {
                 temp <<= 1;
                 multiple <<= 1;
             }
@@ -22,14 +24,9 @@ public:
             result += multiple;
         }
 
-        // Adjust sign
-        if ((dividend < 0) ^ (divisor < 0)) result = -result;
+        if((dividend < 0) ^ (divisor < 0))
+            result = -result;
 
-        // Clamp result in 32-bit range
-        if (result > INT_MAX_LL) return INT_MAX_LL;
-        if (result < INT_MIN_LL) return INT_MIN_LL;
-
-        return result;
-     
+        return (int)result;
     }
 };
