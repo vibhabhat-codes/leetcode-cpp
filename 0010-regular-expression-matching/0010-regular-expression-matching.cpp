3d6700fcd0e6008906975184f543f52deeeecaea
Vibha Bhat <vibhabhat0410@gmail.com>
2026-05-17 00:17:52 +0530

    Time: 0 ms (100%), Space: 9.2 MB (31.14%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int n = s.size();
+        int m = p.size();
+
+        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
+
+        dp[0][0] = true;
+
+        // Handle patterns like a*, a*b*, a*b*c*
+        for (int j = 2; j <= m; j++) {
+            if (p[j - 1] == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+
+                // Normal match or '.'
+                if (s[i - 1] == p[j - 1] || p[j - 1] == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+
+                // '*'
+                else if (p[j - 1] == '*') {
+
+                    // Zero occurrence
+                    dp[i][j] = dp[i][j - 2];
+
+                    // One or more occurrence
+                    if (p[j - 2] == s[i - 1] || p[j - 2] == '.') {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j];
+                    }
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+};
