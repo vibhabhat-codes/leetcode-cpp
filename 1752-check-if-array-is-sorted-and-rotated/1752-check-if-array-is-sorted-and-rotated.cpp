eb2e388675a6e6eda61451fcaf65e782025a8921
Vibha Bhat <vibhabhat0410@gmail.com>
2026-05-23 23:54:30 +0530

    Time: 3 ms (2.6%), Space: 11.2 MB (82.15%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-         int n = nums.size();
-        int count = 0;
 
-        for (int i = 0; i < n; i++) {
-            // Compare current element with next element
-            // % n handles the circular check (last with first)
-            if (nums[i] > nums[(i + 1) % n]) {
+        int count = 0;
+        int n = nums.size();
+
+        for(int i = 0; i < n; i++) {
+
+            if(nums[i] > nums[(i + 1) % n]) {
                 count++;
             }
+        }
 
-            // More than one drop → not rotated sorted
-            if (count > 1) {
-                return false;
-            }}
-                  return true;
-    
-
+        return count <= 1;
     }
 };
